9e379679354f6e908041fd2f8bcb16e69d65da8b
Nikolai <djaknvn@gmail.com>
2022-12-18 23:53:18 +0300

    Посмотрел половвину 2 лекции

---
+++ Exe009_IntroArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2> result) result = arg2;
+    if(arg3> result) result = arg3;
+    return result;
+}
+//              0   1   2   3   4   5   6   7   8
+int[] array = {12, 20, 38, 34, 45, 16, 27, 48, 39};
+array[0] = 12;
+Console.WriteLine(array[4])
+
+int max = Max(
+    Max (a1, b1, c1),
+    Max (a2, b2, c2),
+    Max(a3, b3, c3));
+    
+
+Console.WriteLine(max);
